cb571ccc1c7948d57d2af32ad2817faece7a82c9
hairydynamicist <john.b.murray@gmail.com>
2024-06-27 09:09:14 +0100

    complete to end of lecture 233

--- cybulde/models/backbones.py
+++ cybulde/models/backbones.py
@@ -1,7 +1,6 @@
 from torch import nn
 from transformers import AutoConfig, AutoModel, BatchEncoding
-from transformers.modeling_outputs import BaseModelOutput
-#from transformers.modeling_outputs import BaseModelOutputWithPooling
+from transformers.modeling_outputs import BaseModelOutputWithPooling
 #
 # from cybulde.models.transformations import Transformation
 from cybulde.utils.io_utils import translate_gcs_dir_to_local
@@ -19,43 +18,21 @@ class Backbone(nn.Module):
 
 class HuggingFaceBackbone(Backbone):
     def __init__(
-        self, pretrained_model_name_or_path: str, pretrained: bool = False
+        self, pretrained_model_name_or_path: str, transformation: Transformation, pretrained: bool = False
     ) -> None:
-        super().__init__()
+        super().__init__(transformation)
         self.backbone = self.get_backbone(pretrained_model_name_or_path, pretrained)
 
-    def forward(self, encodings: BatchEncoding) -> BaseModelOutput:
-        output: BaseModelOutput = self.backbone(**encodings)
+    def forward(self, encodings: BatchEncoding) -> BaseModelOutputWithPooling:
+        output: BaseModelOutputWithPooling = self.backbone(**encodings)
         return output
 
     def get_backbone(self, pretrained_model_name_or_path: str, pretrained: bool) -> nn.Module:
         path = translate_gcs_dir_to_local(pretrained_model_name_or_path)
         config = AutoConfig.from_pretrained(path)
         if pretrained:
-            AutoConfig.from_pretrained(path, config=config)
-            return AutoModel.from_pretrained(path, config=config)
-        return AutoModel.from_config(config)
+            backbone_from_pretrained: nn.Module = AutoModel.from_pretrained(path, config=config)
+            return backbone_from_pretrained
 
-
-
-
-# class HuggingFaceBackbone(Backbone):
-#     def __init__(
-#         self, pretrained_model_name_or_path: str, transformation: Transformation, pretrained: bool = False
-#     ) -> None:
-#         super().__init__(transformation)
-#         self.backbone = self.get_backbone(pretrained_model_name_or_path, pretrained)
-#
-#     def forward(self, encodings: BatchEncoding) -> BaseModelOutputWithPooling:
-#         output: BaseModelOutputWithPooling = self.backbone(**encodings)
-#         return output
-#
-#     def get_backbone(self, pretrained_model_name_or_path: str, pretrained: bool) -> nn.Module:
-#         path = translate_gcs_dir_to_local(pretrained_model_name_or_path)
-#         config = AutoConfig.from_pretrained(path)
-#         if pretrained:
-#             backbone_from_pretrained: nn.Module = AutoModel.from_pretrained(path, config=config)
-#             return backbone_from_pretrained
-#
-#         backbone_from_config: nn.Module = AutoModel.from_config(config)
-#         return backbone_from_config
+        backbone_from_config: nn.Module = AutoModel.from_config(config)
+        return backbone_from_config
